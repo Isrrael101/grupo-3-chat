e453c57d97d4c5f29bf69704de0c41a6fd5370b5
Isrrael101 <isrraelfcaq@hotmail.com>
2021-12-17 19:00:31 -0400

    7ta version

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ const socketio = require('socket.io');
 
 const server = http.createServer(app);
 const io = socketio(server);
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
 require('./sockets')(io);
 require('./database');
 
 async function main() {
-  await server.listen(app.get('port'));
-  console.log(`server on port ${app.get('port')}`);
+  await server.listen(app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`);
+  });
 }
 
 main();
